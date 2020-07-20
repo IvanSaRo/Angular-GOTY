cfe9b9f71f86e3dde0be12e8e9b457888d9a62a0
IvanSaRo <ivansancrodr@gmail.com>
2020-07-20 14:59:32 +0200

    getGsmes optimized to charge from cache when already loaded

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -2,16 +2,31 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { environment  } from "../../environments/environment";
 import { Game } from '../interfaces/interfaces';
+import { of } from 'rxjs';
+import { tap } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
+  private games: Game[] = [];
 
   constructor(private http: HttpClient) { }
 
   getGames(){
+    if( this.games.length > 0){
+      //no hay juegos
+     console.log("desde el caché");
+      return of(this.games)//lo devuelve como observable que es lo que pide el componente goty;
+    }else{
+      console.log("desde internet");
+      return this.http.get<Game[]>(`${ environment.url}/api/goty`)
+        .pipe(
+          tap(
+            resp => this.games = resp
+          )
+          )
 
-    return this.http.get<Game[]>(`${ environment.url}/api/goty`)
+    }
   }
 }
